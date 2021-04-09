eddf7158bb8ce214abcba43d354214b01675196c
Vadim Simakin <47472249+vsvad@users.noreply.github.com>
2021-04-09 20:21:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,23 +10,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Python ' + sys.version + '</h1>'
+    return '<h1>Python ' + sys.version + ' on ' + sys.platform + '</h1>'
 
 
 @app.route('/latest.txt')
 def latest():
     soup = BeautifulSoup(requests.get('https://python.org/').text, 'html.parser')
-    tags = soup.find_all('a', class_="button")
-    ftags = []
-    result = '3.8.0'
+    tags = soup.find_all('a')
     for tag in tags:
-        if tag['href'].startswith('https://www.python.org/ftp/python/'):
-            match = re.search('^\d\.\d(\.\d)?', a['href'])
-            if match:
-                span = match.span()
-                result = match.string[span[0]:span[1]]
-                break
-    return Response(result, mimetype="text/plain")
+        match = re.search('^\d\.\d(\.\d)?', a['href'])
+        if match:
+            span = match.span()
+            result = match.string[span[0]:span[1]]
+            return Response(result, mimetype="text/plain")
+    return Response('Unknown', mimetype="text/plain")
 
 
 app.run(host='0.0.0.0', port=os.environ.get('PORT', 8000))
