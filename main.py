55b3eaa7c3ae9b3c9bbdd4bc56851b94cd653866
Vadim Simakin <47472249+vsvad@users.noreply.github.com>
2021-04-09 20:14:14 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import *
+import requests
+import re
+import os
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+
+@app.route('/latest.txt')
+def latest():
+    soup = BeautifulSoup(requests.get('https://python.org/').text, 'html.parser')
+    tags = soup.find_all('a', class_="button")
+    ftags = []
+    result = '3.8.0'
+    for tag in tags:
+        if tag['href'].startswith('https://www.python.org/ftp/python/'):
+            match = re.search('^\d\.\d(\.\d)?', a['href'])
+            if match:
+                span = match.span()
+                result = match.string[span[0]:span[1]]
+                break
+    return Response(result, mimetype="text/plain")
+
+
+app.run(host='0.0.0.0', port=8000)
